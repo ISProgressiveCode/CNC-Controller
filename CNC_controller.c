d0dd154bd81aa04da11170de81563cfaf0e15332
NoHomey <ivo.stratev.tues@gmail.com>
2016-06-29 21:10:47 +0300

    well it works with 2 times but need real test at Raspi with 5+ timers

--- CNC_controller.c
+++ CNC_controller.c
@@ -12,27 +12,52 @@ static enum hrtimer_restart my_callback(struct hrtimer *timer) {
   return axis_controller_change_state(&test);
 }
 
+static struct axis_controller test2;
+
+static enum hrtimer_restart my_callback2(struct hrtimer *timer) {
+  return axis_controller_change_state(&test2);
+}
+
 static int __init on_load(void) {
   printk("on_load\n");
   axis_controller_init(&test, my_callback);
+  axis_controller_init(&test2, my_callback2);
   axis_controller_add_interval(&test, 0, 100000000);
-  axis_controller_add_interval(&test, 0, 10000000);
+  axis_controller_add_interval(&test, 1, 10000000);
   axis_controller_add_interval(&test, 0, 1000000);
-  axis_controller_add_interval(&test, 0, 100000);
+  axis_controller_add_interval(&test, 1, 100000);
   axis_controller_add_interval(&test, 0, 10000);
-  axis_controller_add_interval(&test, 0, 1000);
+  axis_controller_add_interval(&test, 2, 1000);
   axis_controller_add_interval(&test, 0, 100);
-  axis_controller_add_interval(&test, 0, 10);
+  axis_controller_add_interval(&test, 2, 10);
   axis_controller_add_interval(&test, 0, 1);
-  axis_controller_add_interval(&test, 0, 10);
+  axis_controller_add_interval(&test, 3, 10);
   axis_controller_add_interval(&test, 0, 100);
-  axis_controller_add_interval(&test, 0, 1000);
+  axis_controller_add_interval(&test, 2, 1000);
   axis_controller_add_interval(&test, 0, 10000);
-  axis_controller_add_interval(&test, 0, 100000);
+  axis_controller_add_interval(&test, 2, 100000);
   axis_controller_add_interval(&test, 0, 1000000);
-  axis_controller_add_interval(&test, 0, 10000000);
+  axis_controller_add_interval(&test, 1, 10000000);
   axis_controller_add_interval(&test, 0, 100000000);
+  axis_controller_add_interval(&test2, 1, 100000000);
+  axis_controller_add_interval(&test2, 0, 10000000);
+  axis_controller_add_interval(&test2, 2, 1000000);
+  axis_controller_add_interval(&test2, 0, 100000);
+  axis_controller_add_interval(&test2, 2, 10000);
+  axis_controller_add_interval(&test2, 0, 1000);
+  axis_controller_add_interval(&test2, 3, 100);
+  axis_controller_add_interval(&test2, 0, 10);
+  axis_controller_add_interval(&test2, 2, 1);
+  axis_controller_add_interval(&test2, 0, 10);
+  axis_controller_add_interval(&test2, 2, 100);
+  axis_controller_add_interval(&test2, 0, 1000);
+  axis_controller_add_interval(&test2, 1, 10000);
+  axis_controller_add_interval(&test2, 0, 100000);
+  axis_controller_add_interval(&test2, 1, 1000000);
+  axis_controller_add_interval(&test2, 0, 10000000);
+  axis_controller_add_interval(&test2, 1, 100000000);
   axis_controller_controll(&test);
+  axis_controller_controll(&test2);
   return 0;
 }
 
